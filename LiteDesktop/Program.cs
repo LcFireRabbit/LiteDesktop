4da4aaa1ba7ef6d2fc5b1488c76ac2c3651872a7
cheng <15232182039@163.com>
2024-08-11 23:46:10 +0800

    修改

---
+++ LiteDesktop/Program.cs
@@ -0,0 +1,27 @@
+﻿using LiteDesktop.ViewModels;
+using LiteDesktop.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteDesktop
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            App app = new();
+            app.InitializeComponent();
+            MainWindowViewModel mainWindowViewModel = new();
+            MainWindow mainWindow = new()
+            {
+                DataContext = mainWindowViewModel
+            };
+            app.MainWindow = mainWindow;
+            app.Run();
+        }
+    }
+}
---
+++ LiteDesktop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,54 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using Windows.Win32;
+
+namespace LiteDesktop.ViewModels
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        private string _BackgroundImage = string.Empty;
+        /// <summary>
+        /// 背景图片
+        /// </summary>
+        public string BackgroundImage
+        {
+            get => _BackgroundImage;
+            set => SetProperty(ref _BackgroundImage, value);
+        }
+
+
+        public MainWindowViewModel()
+        {
+            GetDesktopBackgroundImage();
+        }
+
+        
+        unsafe void GetDesktopBackgroundImage()
+        {
+            FixedSizeCharStruct wallPaperPath = new FixedSizeCharStruct();
+            bool isSucc = PInvoke.SystemParametersInfo(Windows.Win32.UI.WindowsAndMessaging.SYSTEM_PARAMETERS_INFO_ACTION.SPI_GETDESKWALLPAPER
+                    , 200
+                    , &wallPaperPath
+                    , 0);
+            BackgroundImage = BitConverter.ToString(wallPaperPath.Name);
+        }
+
+        public struct FixedSizeCharStruct
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 200)]
+            public byte[]? Name;
+
+            public FixedSizeCharStruct()
+            {
+
+            }
+        }
+    }
+}
