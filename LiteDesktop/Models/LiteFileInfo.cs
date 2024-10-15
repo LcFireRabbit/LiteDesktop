65b62a62a2bc9d51be5559d46b21f2099484bd94
lianglei <1035040216@qq.com>
2024-10-15 18:22:40 +0800

    修改

--- LiteDesktop/Models/LiteFileInfo.cs
+++ LiteDesktop/Models/LiteFileInfo.cs
@@ -5,15 +5,16 @@ using System.Linq;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace LiteDesktop.Models
 {
     public class LiteFileInfo : ObservableObject
     {
         //icon
-        private Uri? _Icon;
+        private BitmapSource? _Icon;
 
-        public Uri? Icon
+        public BitmapSource? Icon
         {
             get => _Icon;
             set => SetProperty(ref _Icon, value);
@@ -38,12 +39,21 @@ namespace LiteDesktop.Models
         }
 
         //extension
-        private string _Extension;
+        private string? _Extension;
 
-        public string Extension
+        public string? Extension
         {
             get => _Extension;
             set => SetProperty(ref _Extension, value);
+        }
+
+        //CreateTime
+        private DateTime _CreateTime;
+
+        public DateTime CreateTime
+        {
+            get => _CreateTime;
+            set => SetProperty(ref _CreateTime, value);
         }
     }
 }
--- LiteDesktop/ViewModels/MainWindowViewModel.cs
+++ LiteDesktop/ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,18 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using LiteDesktop.Models;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Interop;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace LiteDesktop.ViewModels
 {
@@ -16,32 +20,36 @@ namespace LiteDesktop.ViewModels
     {
         #region Fields
         private const string _defauleBackgroundImage = @"Image\Background\background.jpg";
+
+        private const string _desktopPathKey = "DesktopPath";
         #endregion
 
         #region Properties
-        private Uri _BackgroundImage = new(Path.Combine(Environment.CurrentDirectory, _defauleBackgroundImage)
-            , UriKind.RelativeOrAbsolute);
+        private Uri? _BackgroundImage;
         /// <summary>
         /// 背景图片
         /// </summary>
-        public Uri BackgroundImage
+        public Uri? BackgroundImage
         {
             get => _BackgroundImage;
             set => SetProperty(ref _BackgroundImage, value);
         }
+
+        private List<LiteFileInfo> _LiteFileInfos;
+
+        public List<LiteFileInfo> LiteFileInfos
+        {
+            get => _LiteFileInfos;
+            set => SetProperty(ref _LiteFileInfos, value);
+        }
+
         #endregion
 
         public MainWindowViewModel()
         {
             SetUserBackgroundImage();
 
-            string customDeskPath = Utils.Helpers.AppConfigHelper.GetAppSettingsValue("DesktopPath");
-
-            var diectorys = System.IO.Directory.GetFiles(customDeskPath);
-            foreach (var item in diectorys)
-            {
-                var icon = SetIcon(item);
-            }
+            GetLiteFileInfos();
         }
 
         /// <summary>
@@ -49,21 +57,83 @@ namespace LiteDesktop.ViewModels
         /// </summary>
         private void SetUserBackgroundImage()
         {
-            if (Utils.WindowsApi.User32Extension.GetDesktopWallpaper(out string wallPaperPath))
+            BackgroundImage = Utils.WindowsApi.User32Extension.GetDesktopWallpaper(out string wallPaperPath)
+                ? new Uri(wallPaperPath, UriKind.RelativeOrAbsolute)
+                : new(Path.Combine(Environment.CurrentDirectory, _defauleBackgroundImage), UriKind.RelativeOrAbsolute);
+        }
+
+        private void GetLiteFileInfos()
+        {
+            string customDeskPath = Utils.Helpers.AppConfigHelper.GetAppSettingsValue(_desktopPathKey);
+
+            LiteFileInfos = GetLiteFileInfosByPath(customDeskPath);
+        }
+
+        private List<LiteFileInfo> GetLiteFileInfosByPath(string orderPath)
+        {
+            var fileInfos = new List<LiteFileInfo>();
+            var files = Directory.GetFiles(orderPath);
+            foreach (var item in files)
             {
-                _BackgroundImage = new Uri(wallPaperPath, UriKind.RelativeOrAbsolute);
+                fileInfos.Add(GetLiteFileInfo(item));
+            }
+            var directories = Directory.GetDirectories(orderPath);
+            foreach (var item in directories)
+            {
+                fileInfos.Add(GetLiteFileInfo(item));
+            }
+            return fileInfos;
+        }
+
+        private LiteFileInfo GetLiteFileInfo(string path)
+        {
+            FileInfo fileInfo = new(path);
+            return new LiteFileInfo()
+            {
+                Icon = GetIconFromFilePath(path),
+                FilePath = path,
+                Name = fileInfo.Name,
+                Extension = fileInfo.Extension,
+                CreateTime = fileInfo.CreationTime,
+            };
+        }
+
+        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr ExtractIcon(IntPtr hInst, string lpIconFile, int nIconIndex);
+
+        [DllImport("user32.dll")]
+        private static extern bool DestroyIcon(IntPtr hIcon);
+
+        public static BitmapSource GetIconFromFilePath(string filePath)
+        {
+            // 从文件路径获取图标
+            return GetIcon(filePath, 0);
+        }
+
+        public static BitmapSource GetIconFromFileExtension(string fileExtension)
+        {
+            // 从文件扩展名获取图标
+            return GetIcon(fileExtension, -1);
+        }
+
+        private static BitmapSource GetIcon(string path, int index)
+        {
+            IntPtr hIcon = ExtractIcon(IntPtr.Zero, path, index);
+
+            if (hIcon == IntPtr.Zero)
+            {
+                throw new Exception("无法提取图标");
+            }
+
+            try
+            {
+                using Icon icon = Icon.FromHandle(hIcon);
+                return Imaging.CreateBitmapSourceFromHIcon(icon.Handle, Int32Rect.Empty,BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally
+            {
+                DestroyIcon(hIcon);
             }
         }
-
-        [DllImport("shell32.DLL", EntryPoint = "ExtractAssociatedIcon")]
-        private static extern int ExtractAssociatedIconA(int hInst, string lpIconPath, ref int lpiIcon); //声明函数
-        System.IntPtr thisHandle;
-        public System.Drawing.Icon SetIcon(string path)
-        {
-            int RefInt = 0;
-            thisHandle = new IntPtr(ExtractAssociatedIconA(0, path, ref RefInt));
-            return System.Drawing.Icon.FromHandle(thisHandle);
-        }
-
     }
 }
